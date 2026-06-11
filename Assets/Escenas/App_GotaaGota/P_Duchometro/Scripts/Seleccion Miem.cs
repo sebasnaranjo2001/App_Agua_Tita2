176e658472968bd9f301e38f08c964fd84885fe2
sebasnaranjo2001 <sebas.haro2001@gmail.com>
2026-06-10 23:14:42 -0500

    Arreglo Colores

--- Assets/Escenas/App_GotaaGota/P_Duchometro/Scripts/Seleccion Miem.cs
+++ Assets/Escenas/App_GotaaGota/P_Duchometro/Scripts/Seleccion Miem.cs
@@ -13,15 +13,15 @@ public class SeleccionMiembros : MonoBehaviour
     public Image panelInsignia;
     public TMP_Text textoDuchas;
 
-    private Color colorBaseDeEstaTarjeta;
-    private bool estaInicializado = false;
+    private CanvasGroup canvasGroup;
 
     void Start()
     {
-        if (imagenFondo != null)
+        // Buscamos o agregamos automáticamente el CanvasGroup
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
         {
-            colorBaseDeEstaTarjeta = imagenFondo.color;
-            estaInicializado = true;
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
         }
     }
 
@@ -29,9 +29,9 @@ public class SeleccionMiembros : MonoBehaviour
     {
         if (Avisos.instance != null) Avisos.instance.RegistrarSeleccion(this);
 
-        if (estaInicializado && imagenFondo != null)
+        if (canvasGroup != null)
         {
-            imagenFondo.color = colorBaseDeEstaTarjeta * 0.7f;
+            canvasGroup.alpha = 0.85f;
         }
 
         Debug.Log("Has seleccionado a: " + gameObject.name);
@@ -39,65 +39,56 @@ public class SeleccionMiembros : MonoBehaviour
 
     public void Deseleccionar()
     {
-        if (estaInicializado && imagenFondo != null)
+        if (canvasGroup != null)
         {
-            imagenFondo.color = colorBaseDeEstaTarjeta;
+            canvasGroup.alpha = 1f;
         }
     }
 
-    // --- NUEVA FUNCIÓN: APLICA LOS COLORES SEGÚN EL ÍNDICE ---
     public void AplicarTema(int indiceTema)
     {
         Color colFondo = Color.white;
         Color colTexto = Color.black;
         Color colInsignia = Color.gray;
-        Color colTextoInsignia = Color.white; // Siempre blanco
+        Color colTextoInsignia = Color.white;
 
         switch (indiceTema)
         {
-            case 0: // Kit 2: Bosque
+            case 0:
                 ColorUtility.TryParseHtmlString("#B7E4C7", out colFondo);
                 ColorUtility.TryParseHtmlString("#1B4332", out colTexto);
                 ColorUtility.TryParseHtmlString("#2C6A4D", out colInsignia);
                 break;
-            case 1: // Kit 3: Amanecer
+            case 1:
                 ColorUtility.TryParseHtmlString("#FFD6A5", out colFondo);
                 ColorUtility.TryParseHtmlString("#5B3E35", out colTexto);
                 ColorUtility.TryParseHtmlString("#855631", out colInsignia);
                 break;
-            case 2: // Kit 4: Lavanda
+            case 2:
                 ColorUtility.TryParseHtmlString("#CDB4DB", out colFondo);
                 ColorUtility.TryParseHtmlString("#3C096C", out colTexto);
                 ColorUtility.TryParseHtmlString("#421D61", out colInsignia);
                 break;
-            case 3: // Kit 5: Sandía
+            case 3:
                 ColorUtility.TryParseHtmlString("#FFC8DD", out colFondo);
                 ColorUtility.TryParseHtmlString("#590D22", out colTexto);
                 ColorUtility.TryParseHtmlString("#840C2E", out colInsignia);
                 break;
-            case 4: // Kit 6: Girasol
+            case 4:
                 ColorUtility.TryParseHtmlString("#FFF3B0", out colFondo);
                 ColorUtility.TryParseHtmlString("#523906", out colTexto);
                 ColorUtility.TryParseHtmlString("#916525", out colInsignia);
                 break;
-            default: // Por si hay error, usa el bosque por defecto
+            default:
                 ColorUtility.TryParseHtmlString("#B7E4C7", out colFondo);
                 ColorUtility.TryParseHtmlString("#1B4332", out colTexto);
                 ColorUtility.TryParseHtmlString("#2C6A4D", out colInsignia);
                 break;
         }
 
-        // Aplicamos los colores a los componentes de la tarjeta
-        if (imagenFondo != null)
-        {
-            imagenFondo.color = colFondo;
-            colorBaseDeEstaTarjeta = colFondo; // Actualizamos para que la selección (oscurecer) funcione bien
-            estaInicializado = true;
-        }
-
+        if (imagenFondo != null) imagenFondo.color = colFondo;
         if (textoNombre != null) textoNombre.color = colTexto;
         if (textoEdad != null) textoEdad.color = colTexto;
-
         if (panelInsignia != null) panelInsignia.color = colInsignia;
         if (textoDuchas != null) textoDuchas.color = colTextoInsignia;
     }
